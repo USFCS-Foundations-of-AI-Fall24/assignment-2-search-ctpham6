3946c82fbd63bdf069a228e7e848bd46b9145a9d
Colin Pham <ctpham6@dons.usfca.edu>
2024-09-18 02:37:09 -0700

    Limited Depth Search attempted

--- search_algorithms.py
+++ search_algorithms.py
@@ -39,12 +39,19 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
     search_queue = deque()
     closed_list = {}
     states_generated = 0
+    check_depth_limit_reached = False
+    depth = 1
 
     search_queue.append((startState,""))
     if use_closed_list :
         closed_list[startState] = True
+    if limit > 0 :
+        check_depth_limit_reached = True
     while len(search_queue) > 0 :
         ## this is a (state, "action") tuple
+        if check_depth_limit_reached :
+            if depth > limit :
+                break
         next_state = search_queue.pop()
         states_generated += 1
         if goal_test(next_state[0]):
@@ -64,6 +71,7 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
                 for s in successors :
                     closed_list[s[0]] = True
             search_queue.extend(successors)
+        depth += 1
     print("States Generated: " + str(states_generated))
 ## add iterative deepening search here
 
