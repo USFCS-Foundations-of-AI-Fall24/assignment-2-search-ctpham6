bfd9899c6f9ef6f2549e32cf2fa8d06d6a8c8f97
Colin Pham <ctpham6@dons.usfca.edu>
2024-09-18 23:48:10 -0700

    Prints out ending depth if needed

--- search_algorithms.py
+++ search_algorithms.py
@@ -23,8 +23,8 @@ def breadth_first_search(startState, action_list, goal_test, use_closed_list=Tru
             ptr = next_state[0]
             while ptr is not None :
                 ptr = ptr.prev
-                print(ptr)
                 print("----------")
+                print(ptr)
             return next_state
         else :
             successors = next_state[0].successors(action_list)
@@ -43,7 +43,7 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
     closed_list = {}
     states_generated = 0
     check_depth_limit_reached = False
-    depth = 1
+    depth = 0
 
     search_queue.append((startState,""))
     if use_closed_list :
@@ -55,33 +55,27 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
         next_state = search_queue.pop()
         states_generated += 1
         depth += 1
+        if (check_depth_limit_reached and depth > limit) :
+            break
         if goal_test(next_state[0], subproblem):
-            print("Goal found")
+            if (check_depth_limit_reached) :
+                print("Goal found at depth of " + str(depth))
+            else :
+                print("Goal found")
             print("States Generated: " + str(states_generated))
             print(next_state)
             ptr = next_state[0]
             while ptr is not None :
                 ptr = ptr.prev
-                print(ptr)
                 print("----------")
+                print(ptr)
             return next_state
         else :
-            if check_depth_limit_reached :
-                # Checks to see if going another depth is within the limit
-                if depth + 1 <= limit:
-                    successors = next_state[0].successors(action_list)
-                    if use_closed_list:
-                        successors = [item for item in successors
-                                      if item[0] not in closed_list]
-                        for s in successors:
-                            closed_list[s[0]] = True
-                    search_queue.extend(successors)
-            else :
-                successors = next_state[0].successors(action_list)
-                if use_closed_list :
-                    successors = [item for item in successors
-                                        if item[0] not in closed_list]
-                    for s in successors :
-                        closed_list[s[0]] = True
-                search_queue.extend(successors)
+            successors = next_state[0].successors(action_list)
+            if use_closed_list :
+                successors = [item for item in successors
+                                    if item[0] not in closed_list]
+                for s in successors :
+                    closed_list[s[0]] = True
+            search_queue.extend(successors)
     print("States Generated: " + str(states_generated))
