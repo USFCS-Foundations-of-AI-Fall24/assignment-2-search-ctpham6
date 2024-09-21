1b885043a9d104297dd1423cc8f067e3ee46f709
Colin Pham <ctpham6@dons.usfca.edu>
2024-09-21 02:38:49 -0700

    Solver not solving

--- mapcoloring.py
+++ mapcoloring.py
@@ -23,16 +23,6 @@ Solano = model.NewIntVar(0,2,'Solano')
 Napa = model.NewIntVar(0,2,'Napa')
 Sonoma = model.NewIntVar(0,2,'Sonoma')
 
-# Antenna 1 is adjacent to 2,3 and 4.
-# Antenna 2 is adjacent to 1, 3, 5, and 6
-# Antenna 3 is adjacent to 1, 2, 6, and 9
-# Antenna 4 is adjacent to 1, 2, and 5.
-# Antenna 5 is adjacent to 2 and 4
-# Antenna 6 is adjacent to 2, 7 and 8
-# Antenna 7 is adjacent to 6 and 8
-# Antenna 8 is adjacent to 7 and 9
-# Antenna 9 is adjacent to 3 and 8
-
 Antenna1 = model.NewIntVar(0,2, "A1")
 Antenna2 = model.NewIntVar(0,2, "A2")
 Antenna3 = model.NewIntVar(0,2, "A3")
@@ -57,19 +47,47 @@ model.Add(Napa != Solano)
 model.Add(Solano != ContraCosta)
 model.Add(ContraCosta != Marin)
 
+# Antenna 1 is adjacent to 2,3 and 4.
+antenna_model.Add(Antenna1 != Antenna2)
+antenna_model.Add(Antenna1 != Antenna3)
+antenna_model.Add(Antenna1 != Antenna4)
+# Antenna 2 is adjacent to 1, 3, 5, and 6
+antenna_model.Add(Antenna2 != Antenna3)
+antenna_model.Add(Antenna2 != Antenna5)
+antenna_model.Add(Antenna2 != Antenna6)
+# Antenna 3 is adjacent to 1, 2, 6, and 9
+antenna_model.Add(Antenna3 != Antenna6)
+antenna_model.Add(Antenna3 != Antenna9)
+# Antenna 4 is adjacent to 1, 2, and 5.
+antenna_model.Add(Antenna4 != Antenna5)
+# Antenna 6 is adjacent to 2, 7 and 8
+antenna_model.Add(Antenna6 != Antenna7)
+antenna_model.Add(Antenna6 != Antenna8)
 
 
 status = solver.Solve(model)
 
-if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-    print("SF: %s" % colors[solver.Value(SF)])
-    print("Alameda: %s" % colors[solver.Value(Alameda)])
-    print("Marin: %s" % colors[solver.Value(Marin)])
-    print("Contra Costa: %s" % colors[solver.Value(ContraCosta)])
-    print("Solano: %s" % colors[solver.Value(Solano)])
-    print("Sonoma: %s" % colors[solver.Value(Sonoma)])
-    print("Santa Clara: %s" % colors[solver.Value(SantaClara)])
-    print("San Mateo: %s" % colors[solver.Value(SanMateo)])
-    print("Napa: %s" % colors[solver.Value(Napa)])
+antenna_status = antenna_solver.Solve(antenna_model)
 
+# if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+#     print("SF: %s" % colors[solver.Value(SF)])
+#     print("Alameda: %s" % colors[solver.Value(Alameda)])
+#     print("Marin: %s" % colors[solver.Value(Marin)])
+#     print("Contra Costa: %s" % colors[solver.Value(ContraCosta)])
+#     print("Solano: %s" % colors[solver.Value(Solano)])
+#     print("Sonoma: %s" % colors[solver.Value(Sonoma)])
+#     print("Santa Clara: %s" % colors[solver.Value(SantaClara)])
+#     print("San Mateo: %s" % colors[solver.Value(SanMateo)])
+#     print("Napa: %s" % colors[solver.Value(Napa)])
+
+if antenna_status == cp_model.OPTIMAL or antenna_status == cp_model.FEASIBLE:
+    print("A1: %s" % frequencies[solver.Value(Antenna1)])
+    print("A2: %s" % frequencies[solver.Value(Antenna2)])
+    print("A3: %s" % frequencies[solver.Value(Antenna3)])
+    print("A4: %s" % frequencies[solver.Value(Antenna4)])
+    print("A5: %s" % frequencies[solver.Value(Antenna5)])
+    print("A6: %s" % frequencies[solver.Value(Antenna6)])
+    print("A7: %s" % frequencies[solver.Value(Antenna7)])
+    print("A8: %s" % frequencies[solver.Value(Antenna8)])
+    print("A9: %s" % frequencies[solver.Value(Antenna9)])
 
