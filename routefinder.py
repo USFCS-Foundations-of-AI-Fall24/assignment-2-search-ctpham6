88dbb89a9e90bb4167c00e2ac8b250ba946cb5d9
Colin Pham <ctpham6@dons.usfca.edu>
2024-09-20 20:30:46 -0700

    a_star and ucs implemented

--- routefinder.py
+++ routefinder.py
@@ -48,25 +48,25 @@ def a_star(start_state, heuristic_fn, goal_test, use_closed_list=True, ucs = Fal
     if use_closed_list :
         closed_list[start_state] = True
     while next_state :
-        print("Location is: " + str(next_state.location))
         if goal_test(next_state) :
-            print("goal found")
+            print("[Goal Found]")
+            ptr = next_state
+            print("Steps Taken:")
+            while ptr :
+                print(ptr)
+                ptr = ptr.prev_state
             print("States Generated: " + str(states_generated))
             return next_state
         else :
             for edge in next_state.mars_graph.get_edges(next_state.location):
-                if ucs :
-                    state_to_enqueue = map_state(location=edge.dest, mars_graph=next_state.mars_graph, prev_state=next_state,
-                                                 g=next_state.g + 1, h=h1(next_state))
-                else :
-                    state_to_enqueue = map_state(location=edge.dest, mars_graph=next_state.mars_graph, prev_state=next_state,
-                                                 g=next_state.g + 1, h=sld(next_state))
+                state_to_enqueue = map_state(location=edge.dest, mars_graph=next_state.mars_graph, prev_state=next_state,
+                                                 g=1, h=heuristic_fn(next_state))
                 if use_closed_list :
                     try :
                         closed_list[state_to_enqueue]
                     except :
-                        search_queue.put(state_to_enqueue)
                         closed_list[state_to_enqueue] = True
+                        search_queue.put(state_to_enqueue)
                         states_generated += 1
                 else :
                     search_queue.put(state_to_enqueue)
@@ -112,4 +112,8 @@ def mission_complete(state, sub_problem = False) :
 if __name__=="__main__" :
 
     mars_map_state = map_state(location="1,1", mars_graph = read_mars_graph("MarsMap.txt"))
+    print("a*: ")
     a_star(mars_map_state, sld, mission_complete)
+    print("")
+    print("uniform cost search: ")
+    a_star(mars_map_state, h1, mission_complete)
