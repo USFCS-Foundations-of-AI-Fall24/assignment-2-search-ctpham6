5d9b615aeade7fb2c4cd5923f5542d4ac16a8e26
ctpham6 <123421596+ctpham6@users.noreply.github.com>
2024-09-23 16:54:18 -0700

    Added DLS Description

--- submission.py
+++ submission.py
@@ -7,12 +7,13 @@ import mapcoloring
 #
 # First will be mars_planner.py
 # The searches ran include (in order), BFS, BFS subproblems (3), DFS, DFS subproblems
-# DLS is also available, but I wanted to only include the searches with no constraints
 # The output of mars_planner.py is as such:
 #     Search name (like Default Condition BFS)
 #     Whether a goal was found or not
 #     The end state along with its previous states
 #     The states generated (and depth if DFS)
+# Lastly, marsplanner.py will output the results of a regular DFS. The catch is that it will then be compared to the
+# results of a DLS. Very interesting results here. A regular DFS goes to the depth of 11 while a DLS goes to 10.
 #
 # The second is the routefinder.py
 # A* will run first, tell you if the goal has been found, and prints the steps taken, and finally the states
